a2eaf88f370a355234a6a0b809303a05c6b310d7
Lib <ybanezjosevincent.bshm.cpc@gmail.com>
2025-08-04 16:49:14 +0800

    Update Loader.Randomizer.lua

--- Randomizer/Loader.Randomizer.lua
+++ Randomizer/Loader.Randomizer.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://pastefy.app/kNe1bjyy/raw"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptsHubsIds/SerosScripts/refs/heads/main/AutoPetLevel.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptsHubsIds/SerosScript/refs/heads/main/Loader/LoaderV3.lua"))()
